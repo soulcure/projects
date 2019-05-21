a803e5b8b0771c698bca5d08993cba317f614a05
colin <287268221@qq.com>
2019-05-21 17:04:49 +0800

    update

--- go_charger/charge_api_tcp/charge_manager/src/db/db.go
+++ go_charger/charge_api_tcp/charge_manager/src/db/db.go
@@ -4,88 +4,91 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"util"
 	"os"
 	"sync"
 	"time"
+	"util"
 )
-const connsnum int=32
-type DbInstance struct{
-	Db  *sql.DB
+
+const connsnum int = 32
+
+type DbInstance struct {
+	Db   *sql.DB
 	busy bool
 }
-type DatabasePool struct{
+type DatabasePool struct {
 	instance []*DbInstance
-	num  int
-	user    string
-	password  string
-	addr      string
-	name        string
+	num      int
+	user     string
+	password string
+	addr     string
+	name     string
 
 	sync.Mutex
 }
 
-var DbPool  *DatabasePool
-func init(){
+var DbPool *DatabasePool
+
+func init() {
 	addr := util.CFG.Section("db").Key("addr").String()
 	user := util.CFG.Section("db").Key("user").String()
 	passwd := util.CFG.Section("db").Key("password").String()
-	name:=util.CFG.Section("db").Key("dbname").String()
-	num,err:= util.CFG.Section("db").Key("conns").Int()
-	if err !=nil{
-		num=connsnum
+	name := util.CFG.Section("db").Key("dbname").String()
+	num, err := util.CFG.Section("db").Key("conns").Int()
+	if err != nil {
+		num = connsnum
 	}
-	DbPool =&DatabasePool{
-		user:user,
-		password:passwd,
-		addr:addr,
-		name:name,
-		num:num,
+	DbPool = &DatabasePool{
+		user:     user,
+		password: passwd,
+		addr:     addr,
+		name:     name,
+		num:      num,
 	}
-	DbPool.instance=make([]*DbInstance,num)
-	for i:=0;i<num;i++{
-		DbPool.instance[i]=&DbInstance{
+	DbPool.instance = make([]*DbInstance, num)
+	for i := 0; i < num; i++ {
+		DbPool.instance[i] = &DbInstance{
 			nil,
 			false,
 		}
 	}
-	if err=DbPool.Conn();err!=nil{
+	if err = DbPool.Conn(); err != nil {
 		log.Println(err)
 		os.Exit(0)
 	}
 	log.Println("connect db success")
 }
 
-func (pool* DatabasePool) Conn() error{
-	dataSourceName:=pool.user+":"+pool.password+"@tcp("+pool.addr+")/"+pool.name
-	for i:=0;i<pool.num;i++{
-		d , err := sql.Open("mysql", dataSourceName)
+func (pool *DatabasePool) Conn() error {
+	dataSourceName := pool.user + ":" + pool.password + "@tcp(" + pool.addr + ")/" + pool.name
+	for i := 0; i < pool.num; i++ {
+		d, err := sql.Open("mysql", dataSourceName)
 		if err != nil {
-			return err  // Just for example purpose. You should use proper error handling instead of panic
+			return err // Just for example purpose. You should use proper error handling instead of panic
 		}
-		pool.instance[i].Db=d
+		pool.instance[i].Db = d
 	}
 	return nil
 }
 
-func (db* DatabasePool)getDb()* DbInstance {
+func (db *DatabasePool) getDb() *DbInstance {
 	db.Lock()
 	defer db.Unlock()
-	for _,instance:=range db.instance{
-		if !instance.busy{
-			instance.busy=true
+	for _, instance := range db.instance {
+		if !instance.busy {
+			instance.busy = true
 			return instance
 		}
 	}
 	return nil
 }
 
-func (pool* DatabasePool)GetDb()* DbInstance{
-	instance:=pool.getDb()
-	if instance==nil{
-		time.Sleep(50*time.Millisecond)
-		instance=pool.getDb()
-		if instance==nil{
+func (pool *DatabasePool) GetDb() *DbInstance {
+	instance := pool.getDb()
+	if instance == nil {
+		time.Sleep(50 * time.Millisecond)
+		instance = pool.getDb()
+		if instance == nil {
 			log.Println("db is busy,get db instance fail")
 			return nil
 		}
@@ -93,7 +96,6 @@ func (pool* DatabasePool)GetDb()* DbInstance{
 	return instance
 }
 
-func (instance *DbInstance) Release(){
-	instance.busy=false
+func (instance *DbInstance) Release() {
+	instance.busy = false
 }
-
